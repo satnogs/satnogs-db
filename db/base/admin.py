0fd6a96beab631685ddc662e639048da08e34895
Nikos Roussos <nikos@roussos.cc>
2015-05-09 11:39:14 +0300

    Add admin reverse url for suggestion/transponders relation

--- db/base/admin.py
+++ db/base/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.core.urlresolvers import reverse
 
 from db.base.models import Satellite, Transponder, Suggestion
 
@@ -23,9 +24,21 @@ class SuggestionAdmin(admin.ModelAdmin):
                     'uplink_high', 'downlink_low', 'downlink_high',)
     search_fields = ('satellite', 'uuid',)
     list_filter = ('mode', 'invert')
+    readonly_fields = ('uuid', 'satellite', 'transponder', 'approved', 'user',
+                       'citation', 'transponder_data')
 
     def transponder_uuid(self, obj):
         try:
             return obj.transponder.uuid
         except:
             return '-'
+
+    def transponder_data(self, obj):
+        if obj.transponder:
+            redirect_url = reverse('admin:base_transponder_changelist')
+            extra = '{0}'.format(obj.transponder.pk)
+            return '<a href="{}">Trnasponder Initial Data</a>'.format(
+                redirect_url + extra)
+        else:
+            return '-'
+    transponder_data.allow_tags = True
