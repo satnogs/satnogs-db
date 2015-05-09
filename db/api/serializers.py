1071e663eb38a3981cea047c1b2e24d6e119f94d
Nikos Roussos <nikos@roussos.cc>
2015-05-09 12:52:24 +0300

    Change norad API field to proper name

--- db/api/serializers.py
+++ db/api/serializers.py
@@ -9,13 +9,13 @@ class SatelliteSerializer(serializers.ModelSerializer):
 
 
 class TransponderSerializer(serializers.ModelSerializer):
-    norad = serializers.SerializerMethodField()
+    norad_cat_id = serializers.SerializerMethodField()
 
     class Meta:
         model = Transponder
         fields = ('uuid', 'description', 'alive', 'uplink_low', 'uplink_high',
                   'downlink_low', 'downlink_high', 'mode', 'invert',
-                  'baud', 'norad')
+                  'baud', 'norad_cat_id')
 
-    def get_norad(self, obj):
+    def get_norad_cat_id(self, obj):
         return obj.satellite.norad_cat_id
