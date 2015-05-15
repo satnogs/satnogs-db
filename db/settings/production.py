5340098245ee24dce7744ccd475d848929a3d2a8
Nikos Roussos <nikos@roussos.cc>
2015-05-15 11:47:58 +0300

    Add EMAIL_TIMEOUT setting

--- db/settings/production.py
+++ db/settings/production.py
@@ -11,8 +11,9 @@ SECURE_BROWSER_XSS_FILTER = True
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*')
 
 # Email
-EMAIL_HOST = "localhost"
+EMAIL_HOST = 'localhost'
 EMAIL_PORT = 25
+EMAIL_TIMEOUT = 300
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@example.com')
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
--- db/settings/stage.py
+++ db/settings/stage.py
@@ -1,13 +1,14 @@
 import os
 from base import *
 
-ENVIRONMENT = 'production'
+ENVIRONMENT = 'stage'
 
 # Security
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*')
 
 # Mail
-EMAIL_HOST = "localhost"
+EMAIL_HOST = 'localhost'
 EMAIL_PORT = 25
+EMAIL_TIMEOUT = 300
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@example.com')
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
