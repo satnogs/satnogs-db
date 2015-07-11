b3e404f6fb221cfe5e6e5d826c92f7dc71ec9aae
Nikos Roussos <nikos@roussos.cc>
2015-07-11 21:25:16 +0300

    Add active template tag

---
+++ db/base/templatetags/tags.py
@@ -0,0 +1,14 @@
+import re
+
+from django import template
+from django.core.urlresolvers import reverse
+
+register = template.Library()
+
+
+@register.simple_tag
+def active(request, urls):
+    if request.path in ( reverse(url) for url in urls.split() ):
+        return 'active'
+    return None
+
