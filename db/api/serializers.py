79a5bcdc1c651a5ce169d21897bb18f8364d8100
Nikos Roussos <nikos@roussos.cc>
2015-08-14 16:57:39 +0300

    API minor fixes
    * Expose mode_id instead of mode
    * Expose satellite names and image

--- db/api/serializers.py
+++ db/api/serializers.py
@@ -12,17 +12,24 @@ class ModeSerializer(serializers.ModelSerializer):
 class SatelliteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Satellite
-        fields = ('norad_cat_id', 'name')
+        fields = ('norad_cat_id', 'name', 'names', 'image')
 
 
 class TransmitterSerializer(serializers.ModelSerializer):
     norad_cat_id = serializers.SerializerMethodField()
+    mode_id = serializers.SerializerMethodField()
 
     class Meta:
         model = Transmitter
         fields = ('uuid', 'description', 'alive', 'uplink_low', 'uplink_high',
-                  'downlink_low', 'downlink_high', 'mode', 'invert',
+                  'downlink_low', 'downlink_high', 'mode_id', 'invert',
                   'baud', 'norad_cat_id')
+
+    def get_mode_id(self, obj):
+        try:
+            return obj.mode.id
+	except:
+	    return None
 
     def get_norad_cat_id(self, obj):
         return obj.satellite.norad_cat_id
