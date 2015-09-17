62e48dc6597bb6b7be358ae460ba875c554dbcec
Nikos Roussos <nikos@roussos.cc>
2015-09-17 12:57:38 +0300

    [Re: #52] Email admins on new suggestion

--- db/base/views.py
+++ db/base/views.py
@@ -8,6 +8,9 @@ from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseNotFound, HttpResponseServerError, HttpResponse
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
 
 from db.base.models import Mode, Transmitter, Satellite, Suggestion
 from db.base.forms import SatelliteSearchForm, SuggestionForm
@@ -63,6 +66,30 @@ def suggestion(request):
         suggestion = suggestion_form.save(commit=False)
         suggestion.user = request.user
         suggestion.save()
+
+        # Notify admins
+        admins = User.objects.filter(is_superuser=True)
+        site = get_current_site(request)
+        subject = '[{0}] A new suggestion was submitted'.format(site.name)
+        template = 'emails/new_suggestion.txt'
+        saturl = '{0}{1}'.format(
+            site.domain,
+            reverse('satellite', kwargs={'norad': suggestion.satellite.norad_cat_id})
+        )
+        data = {
+            'satname': suggestion.satellite.name,
+            'saturl': saturl,
+            'site_name': site.name
+        }
+        message = render_to_string(template, {'data': data})
+        for user in admins:
+            try:
+                user.email_user(subject, message, from_email=settings.DEFAULT_FROM_EMAIL)
+            except:
+                logger.error(
+                    'Could not send email to user',
+                    exc_info=True
+                )
 
         messages.success(request, ('Your suggestion was stored successfully. '
                                    'Thanks for contibuting!'))
