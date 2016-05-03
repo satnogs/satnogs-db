118300f6a4b9475032507d3d7e04421401eb2ec8
keikreutler <kei@ourmachine.net>
2016-05-03 18:50:53 +0200

    Added d3 viz as backbone view

--- db/static/js/app.backbone.js
+++ db/static/js/app.backbone.js
@@ -92,3 +92,146 @@ telemetryData.fetch();
 var telemetryTimeView = new TelemetryTimeView({ collection: telemetryData });
 telemetryTimeView.render();
 
+
+
+/************************************
+D3 Collection
+************************************/
+
+/*var TelemetryViz = Backbone.View.extend({
+
+  "el": "#telemetry-viz",
+
+  initialize: function() {
+    // Container el
+  }
+
+  render: function() {
+    // On changes in collection, render
+  }
+
+  frame: function() {
+    // on time draw
+  },
+ 
+
+})*/
+
+
+var w = 440,
+    h = 200;
+
+
+var DataPoint = Backbone.Model.extend({
+
+    initialize: function(x) {
+        this.set({
+            x: x
+        });
+    },
+
+    type: "point",
+
+    randomize: function() {
+        this.set({
+            x: Math.round(Math.random() * 10)
+        });
+    }
+
+});
+
+var DataSeries = Backbone.Collection.extend({
+
+    model: Telemetry,
+
+    fetch: function() {
+        this.reset();
+        this.add([
+            new DataPoint(10),
+            new DataPoint(12),
+            new DataPoint(15),
+            new DataPoint(18)
+            ]);
+    },
+
+});
+
+var BarGraph = Backbone.View.extend({
+
+    "el": "#graph",
+
+    initialize: function() {
+
+        _.bindAll(this, "render");
+        this.collection.bind("change", this.render);
+
+        this.chart = d3.selectAll($(this.el)).append("svg").attr("class", "chart").attr("width", w).attr("height", h).append("g").attr("transform", "translate(10,15)");
+
+        this.collection.fetch();
+    },
+
+    render: function() {
+
+        var data = this.collection.models;
+
+        var x = d3.scale.linear().domain([0, d3.max(data.damod_data.EPS_V, function(d) {
+            return d.get("x");
+        })]).range([0, w - 10]);
+
+        var y = d3.scale.ordinal().domain([0, 1, 2, 3]).rangeBands([0, h - 20]);
+
+        var self = this;
+        var rect = this.chart.selectAll("rect").data(data.damod_data, function(d, i) {
+            return i;
+        });
+
+        rect.enter().insert("rect", "text").attr("y", function(d) {
+            return y(d.get("x"));
+        }).attr("width", function(d) {
+            return x(d.get("x"));
+        }).attr("height", y.rangeBand());
+
+        rect.transition().duration(1000).attr("width", function(d) {
+            return x(d.get("x"));
+        }).attr("height", y.rangeBand());
+
+        rect.exit().remove();
+        
+        var text = this.chart.selectAll("text").data(data.damod_data, function(d, i) {
+            return i;
+        });
+
+       text.enter().append("text")
+        .attr("x", function(d) {
+            return x(d.get("x"));
+        })
+        .attr("y", function(d,i) { return y(i) + y.rangeBand() / 2; })
+        .attr("dx", -3) // padding-right
+        .attr("dy", ".35em") // vertical-align: middle
+        .attr("text-anchor", "end") // text-align: right
+           .text(function(d) { return d.get("x");});
+   
+        /*this.chart.selectAll("line").data(x.ticks(10)).enter().append("line").attr("x1", x).attr("x2", x).attr("y1", 0).attr("y2", h - 10).style("stroke", "#ccc");
+
+        this.chart.selectAll("line").data(x.ticks(10)).attr("x1", x).attr("x2", x);
+
+        this.chart.selectAll("line").data(x.ticks(10)).exit().remove();
+
+        this.chart.selectAll(".rule").data(x.ticks(10)).enter().append("text").attr("class", "rule").attr("x", x).attr("y", 0).attr("dy", -3).attr("text-anchor", "middle").text(String);
+
+        this.chart.selectAll(".rule").data(x.ticks(10)).attr("x1", x).text(String);
+
+        this.chart.selectAll(".rule").data(x.ticks(10)).exit().remove();*/
+    },
+
+
+});
+
+
+
+    var dataSeries = new DataSeries();
+    console.log(telemetryData);
+    new BarGraph({
+        collection: telemetryData
+    }).render();
+
