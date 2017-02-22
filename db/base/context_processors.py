2c48c02cfd6796422f300a02db83906fa44df3a6
Nikos Roussos <nikos@roussos.cc>
2017-02-22 17:39:26 +0200

    Simplify GA code

--- db/base/context_processors.py
+++ db/base/context_processors.py
@@ -5,9 +5,7 @@ from django.template.loader import render_to_string
 def analytics(request):
     """Returns analytics code."""
     if settings.ENVIRONMENT == 'production':
-        return {'analytics_code': render_to_string('includes/analytics.html',
-                {'google_analytics_key': settings.GOOGLE_ANALYTICS_KEY,
-                 'user': request.user})}
+        return {'analytics_code': render_to_string('includes/analytics.html')}
     else:
         return {'analytics_code': ''}
 
